c148ba42c690297fdbcc770781705aa743b567b1
Emilie M. <emilie.marion.simplon@gmail.com>
2025-06-25 16:00:47 +0200

    added alt + formatted code + rearranged code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 const btn = document.getElementById("send-participation");
 
 btn.addEventListener("click", function () {
-    const checkbox = document.getElementById("register-data-form");
-    if (checkbox.checked){
-        alert("Les participations ne sont pas encore ouvertes.");
-    }
-    else{
-        alert("Merci d'accepter le partage des données avant d'envoyer la participation");
-    }
+  const checkbox = document.getElementById("register-data-form");
+  if (checkbox.checked) {
+    alert("Les participations ne sont pas encore ouvertes.");
+    btn.disabled = true;
+  } else {
+    alert(
+      "Merci d'accepter le partage des données avant d'envoyer la participation"
+    );
+  }
 });
